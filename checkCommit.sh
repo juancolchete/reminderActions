d3cd5435662f8c5c13db34c5461858a3b4cb23b1
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-25 16:03:51 -0300

    ci: check commits in a day from streakCounter contributions

--- checkCommit.sh
+++ checkCommit.sh
@@ -12,20 +12,19 @@ COUNT=$(echo "$EVENTS" \
   | jq --arg TODAY "$TODAY" '
       .[] | select(.date == $TODAY) | .contributionCount
     ')
-echo $COUNT
-# COMMIT_META=$(($3 - COUNT))
-# echo "" > dailyCommit.md
-# echo "# Daily commitment info" >> dailyCommit.md
-# echo "Total commited today: $COUNT" >> dailyCommit.md
+COMMIT_META=$(($3 - COUNT))
+echo "" > dailyCommit.md
+echo "# Daily commitment info" >> dailyCommit.md
+echo "Total commited today: $COUNT" >> dailyCommit.md
 
-# if [ "$COUNT" -lt $3 ]; then
-#   curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> Missing '"$COMMIT_META"' commits"}'
-#   echo "Total to commit: $COMMIT_META" >> dailyCommit.md
-# elif [ "$COUNT" -gt 0 ]; then
-#   echo "Total to commit: 0" >> dailyCommit.md
-#   echo "✅ Yes! You committed today ($COUNT push events)."
-# else
-#   echo "Total to commit: $COMMIT_META" >> dailyCommit.md
-#   echo "❌ No commits found for today ($TODAY)."
-#   curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> You need to commit soon of a bitch"}'
-# fi
+if [ "$COUNT" -lt $3 ]; then
+  curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> Missing '"$COMMIT_META"' commits"}'
+  echo "Total to commit: $COMMIT_META" >> dailyCommit.md
+elif [ "$COUNT" -gt 0 ]; then
+  echo "Total to commit: 0" >> dailyCommit.md
+  echo "✅ Yes! You committed today ($COUNT push events)."
+else
+  echo "Total to commit: $COMMIT_META" >> dailyCommit.md
+  echo "❌ No commits found for today ($TODAY)."
+  curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> You need to commit soon of a bitch"}'
+fi
