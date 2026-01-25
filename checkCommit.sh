c71d6d4a13509839d3e0efafe625c6c966b5a50f
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-25 15:56:49 -0300

    ci: echo count on reminder action

--- checkCommit.sh
+++ checkCommit.sh
@@ -5,26 +5,27 @@ USERNAME="$1"
 TODAY=$(date -u +"%Y-%m-%d" -d "-3 hours")
 
 # Fetch your most recent events (public only)
-EVENTS=$(curl -s "https://api.github.com/users/$USERNAME/events")
+EVENTS=$(curl -s "https://raw.githubusercontent.com/ColcheteDAO/streakCounter/main/$USERNAME/contributions/$USERNAME.json")
 
 # Count PushEvents from today
 COUNT=$(echo "$EVENTS" \
   | jq --arg TODAY "$TODAY" '
-      map(select(.type=="PushEvent" and (.created_at | startswith($TODAY)))) | length
+      .[] | select(.date < $TODAY) | .contributionCount
     ')
-COMMIT_META=$(($3 - COUNT))
-echo "" > dailyCommit.md
-echo "# Daily commitment info" >> dailyCommit.md
-echo "Total commited today: $COUNT" >> dailyCommit.md
+echo $COUNT
+# COMMIT_META=$(($3 - COUNT))
+# echo "" > dailyCommit.md
+# echo "# Daily commitment info" >> dailyCommit.md
+# echo "Total commited today: $COUNT" >> dailyCommit.md
 
-if [ "$COUNT" -lt $3 ]; then
-  curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> Missing '"$COMMIT_META"' commits"}'
-  echo "Total to commit: $COMMIT_META" >> dailyCommit.md
-elif [ "$COUNT" -gt 0 ]; then
-  echo "Total to commit: 0" >> dailyCommit.md
-  echo "✅ Yes! You committed today ($COUNT push events)."
-else
-  echo "Total to commit: $COMMIT_META" >> dailyCommit.md
-  echo "❌ No commits found for today ($TODAY)."
-  curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> You need to commit soon of a bitch"}'
-fi
+# if [ "$COUNT" -lt $3 ]; then
+#   curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> Missing '"$COMMIT_META"' commits"}'
+#   echo "Total to commit: $COMMIT_META" >> dailyCommit.md
+# elif [ "$COUNT" -gt 0 ]; then
+#   echo "Total to commit: 0" >> dailyCommit.md
+#   echo "✅ Yes! You committed today ($COUNT push events)."
+# else
+#   echo "Total to commit: $COMMIT_META" >> dailyCommit.md
+#   echo "❌ No commits found for today ($TODAY)."
+#   curl --location "$2" --header 'Content-Type: application/json' --data '{"content":"<@464919571304939520> You need to commit soon of a bitch"}'
+# fi
